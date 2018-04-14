3d7519d6ef153836e5345a3f80e05566dd49c5ce
Stephen Kurlow <skurlow@gmail.com>
2018-04-15 08:28:39 +1000

    Draw a straight line.

--- app/src/main/java/com/skurlow/Canvas.java
+++ app/src/main/java/com/skurlow/Canvas.java
@@ -33,8 +33,54 @@ public class Canvas {
         }
     }
 
+    /**
+     * Draw a straight line either in a horizontal or vertical direction.
+     *
+     * Diagonal lines are not currently supported.
+     *
+     * @param x1 the x coordinate of the first point on the line.
+     * @param y1 the y coordinate of the first point on the line.
+     * @param x2 the x coordinate of the last point on the line.
+     * @param y2 the y coordinate of the last point on the line.
+     */
     public void drawLine(int x1, int y1, int x2, int y2) {
+        if (x1 != x2 && y1 != y2) {
+            throw new IllegalArgumentException("Horizontal and vertical lines are currently supported.");
+        }
+        if (x1 < 1 || x1 > width) {
+            throw new IllegalArgumentException("The first point must have an x-coordinate within the canvas area.");
+        }
+        if (y1 < 1 || y1 > height) {
+            throw new IllegalArgumentException("The first point must have a y-coordinate within the canvas area.");
+        }
+        if (x2 < 1 || x2 > width) {
+            throw new IllegalArgumentException("The last point must have an x-coordinate within the canvas area.");
+        }
+        if (y2 < 1 || y2 > height) {
+            throw new IllegalArgumentException("The last point must have a y-coordinate within the canvas area.");
+        }
+        if (x1 == x2) {
+            // Draw a vertical line.
+        } else {
+            // Draw a horizontal line.
+            for (int position = x1; position <= x2; position++) {
+                fillCell(position, y1, 'x');
+            }
+        }
+    }
 
+    /**
+     * Write a character onto a single point on the canvas.
+     *
+     * Localise translation of a coordinate into the canvas' 0-index array in this one method to avoid potential defects
+     * and simplify code that uses this method.
+     *
+     * @param x the x-coordinate of a point.
+     * @param y the y-coordinate of a point.
+     * @param fillChar the character to write onto the (x,y) point on the canvas.
+     */
+    private void fillCell(int x, int y, char fillChar) {
+        area[y - 1][x - 1] = fillChar;
     }
 
     public void drawRectangle(int x1, int y1, int x2, int y2) {
--- app/src/test/java/com/skurlow/CanvasTest.java
+++ app/src/test/java/com/skurlow/CanvasTest.java
@@ -3,6 +3,7 @@ package com.skurlow;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
@@ -11,7 +12,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
  class CanvasTest {
 
     @Test
-    void should_render_empty_canvas() {
+    void should_create_empty_canvas() {
         Canvas canvas = new Canvas();
         canvas.create(2, 3);
         char[][] expectedArea =  {
@@ -19,6 +20,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
                 {' ', ' '},
                 {' ', ' '}
         };
-        assertThat(canvas.getArea(), equalTo(expectedArea));
+        assertThat(canvas.getArea(), is(equalTo(expectedArea)));
+    }
+
+    @Test
+    void should_draw_horizontal_line() {
+        Canvas canvas = new Canvas();
+        canvas.create(20, 4);
+        canvas.drawLine(1, 2, 6, 2);
+        char[][] expectedArea =  {
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {'x', 'x', 'x', 'x', 'x', 'x', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+        };
+        assertThat(canvas.getArea(), is(equalTo(expectedArea)));
     }
 }
